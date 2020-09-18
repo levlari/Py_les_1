b14e9813c56822e3dcb225bc4d0235c58701e907
Lev <70910988+levlari@users.noreply.github.com>
2020-09-18 13:55:57 +0300

    3d

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
-    'taggit', 
+    'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 
     
 ]
